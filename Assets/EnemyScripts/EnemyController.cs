d67847fd6d0cd148f99d7b5eed5b29ec5e617c3e
Calde <chipneidev@gmail.com>
2020-03-18 18:24:31 +0100

    Enemy Comportamiento Basico - Patrulla Enemigo Entre Varios Puntos (Configurable desde Inspector) - Perseguir Player -Volver Spawn
    *Checkea Distancia con Player para cambiar a Persecucion
    *Checkea Distancia con PosInicialEnemy para Volver a patrulla si se aleja demasiado

--- Assets/EnemyScripts/EnemyController.cs
+++ Assets/EnemyScripts/EnemyController.cs
@@ -7,6 +7,15 @@ using UnityEngine.AI;
 public class EnemyController : MonoBehaviour
 {
 
+
+    enum EstadoEnemigo
+    {
+        Patrulla,
+        PersiguiendoPlayer,
+        VolverSpawn
+    }
+
+
     NavMeshAgent cmpAgent; //Componente del Enemy (IA)
 
     [SerializeField] Transform[] puntosRuta; //Array de puntos a los que irá en ruta 
@@ -14,23 +23,68 @@ public class EnemyController : MonoBehaviour
 
     [SerializeField] GameObject goPosicionesRuta;
 
+    [SerializeField] float distanciaAlerta = 10;
+    [SerializeField] float distanciaVueltaAlSpawn = 20;
+    [SerializeField] float distanciaSeparacionPlayer = 5;
+    Vector3 puntoSpawn;
+
+
+    EstadoEnemigo estadoActual;
+
+    GameObject goPlayer;
+
+    
 
     void Awake()
     {
         cmpAgent = GetComponent<NavMeshAgent>();
         goPosicionesRuta.transform.parent = null;
+
+        goPlayer = GameObject.FindGameObjectWithTag("Player");
     }
 
     private void Start()
     {
+        estadoActual = EstadoEnemigo.Patrulla; //Empieza en Patrulla
+
+        puntoSpawn = this.transform.position; //guardo PosInicial
+
         puntoActualRuta = 0;
-        SetDestination();
+        SetearDireccion();
 
     }
 
 
     // Update is called once per frame
     void Update()
+    {
+
+        if (estadoActual == EstadoEnemigo.Patrulla) //Estado Patrulla, Enemy ejecuta este comportamiento
+        {
+            ComprobarLlegadaDestino();
+
+            ComprobarAlertaPlayer();
+        }
+
+        else if (estadoActual == EstadoEnemigo.PersiguiendoPlayer) //Estado Perseguir Player
+        {
+            PerseguirPlayer();
+            ComprobarAlejarseElPlayer();
+            ComprobarDistanciaVolver();
+        }
+
+
+        else if (estadoActual == EstadoEnemigo.VolverSpawn) //Estado Volver al Spawn
+        {
+
+            ComprobarLlegadaDestino();
+        }
+
+
+    }
+
+
+    void ComprobarLlegadaDestino()
     {
         if (cmpAgent.pathPending == false)
         {
@@ -44,16 +98,74 @@ public class EnemyController : MonoBehaviour
                 {
                     ++puntoActualRuta;
                 }
-                SetDestination();
+                SetearDireccion();
             }
         }
 
     }
 
-
-    void SetDestination()
+    void SetearDireccion()
     {
+        estadoActual = EstadoEnemigo.Patrulla;
+
         Transform destinoActual = puntosRuta[puntoActualRuta];
+
+        cmpAgent.stoppingDistance = 0; //Para que el enemigo llegue hasta el punto
         cmpAgent.SetDestination(destinoActual.position);
     }
+
+
+
+    void ComprobarAlertaPlayer()
+    {
+        float distanciaConPlayer = Vector3.Distance(this.transform.position, goPlayer.transform.position);
+
+        if (distanciaConPlayer < distanciaAlerta)
+        {
+            PerseguirPlayer();
+        }
+
+
+    }
+
+
+    void PerseguirPlayer()
+    {
+        estadoActual = EstadoEnemigo.PersiguiendoPlayer;
+        cmpAgent.stoppingDistance = distanciaSeparacionPlayer; //Para que Enemigo se situe a una distancia del Player (después ejecuta ataque)
+        cmpAgent.SetDestination(goPlayer.transform.position);
+    }
+
+    void ComprobarDistanciaVolver()
+    {
+        float distanciaConSpawn = Vector3.Distance(this.transform.position, puntoSpawn);
+
+        if (distanciaConSpawn > distanciaVueltaAlSpawn)
+        {
+            VolverSpawn();
+        }
+    }
+
+
+    void ComprobarAlejarseElPlayer()
+    {
+        float distanciaConPlayer = Vector3.Distance(this.transform.position, goPlayer.transform.position);
+
+        if (distanciaConPlayer > distanciaAlerta)
+        {
+            estadoActual = EstadoEnemigo.VolverSpawn;
+            puntoActualRuta = 0;
+            VolverSpawn();
+        }
+    }
+
+    void VolverSpawn()
+    {
+        puntoActualRuta = 0;
+        
+        estadoActual = EstadoEnemigo.VolverSpawn;
+        cmpAgent.stoppingDistance = 0;
+        cmpAgent.SetDestination(puntoSpawn);
+    }
+
 }
