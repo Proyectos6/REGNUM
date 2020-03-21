4576ed69967fd5cd98b24ff8aab41c3059934b63
Uliros <uliros@live.com>
2020-03-21 17:21:05 +0100

    cambios leves

--- Assets/Scripts/PlayerScripts/Ataque.cs
+++ Assets/Scripts/PlayerScripts/Ataque.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class Ataque : MonoBehaviour
 {
-    
+    bool OnRange;
+    float tiempoPegando;
+    GameObject Enemy;
+
     void Start()
     {
                 
     }
     void Update()
     {
-        
+        if (OnRange)
+        {
+            OnRange = false;
+        }
+
     }
 }
