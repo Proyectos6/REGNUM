33a209ae5ffd476e50b114fb43c55802230e2213
ChunisTV <chunis334@gmail.com>
2020-03-28 20:51:39 +0100

    Menu Principal Funcional y menu Pause Funcional

---
+++ Assets/REGNUM/Scripts/Menu/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    public bool paused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(paused)
+        {
+            Time.timeScale = 0;
+
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
---
+++ Assets/REGNUM/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public string nameNextScene;
+    // Start is called before the first frame update
+    
+
+    public void CambiarEscena()
+    { 
+        if(nameNextScene != "Exit")
+        {
+            SceneManager.LoadScene(nameNextScene);
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+}
