1225d5bb511e411dbacf6f353da00eb1a760fa22
Uliros <uliros@live.com>
2020-03-29 10:07:09 +0200

    Dañar enemigo 1

---
+++ Assets/REGNUM/Scripts/PlayerScripts/Dañar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dañar : MonoBehaviour
+{
+
+    EnemyVida VIDA;
+    public GameObject Hacha1H;
+    public GameObject Hacha2H;
+
+    float Daño;
+    float Impulse;
+
+    public float DañoAxe1H;
+    public float DañoAxe2H;
+    public float ImpulseAxe1H;
+    public float ImpulseAxe2H;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (Hacha1H.active)
+        {
+            Daño = DañoAxe1H;
+            Impulse = ImpulseAxe1H;
+        }
+        if (Hacha2H.active)
+        {
+            Daño = DañoAxe2H;
+            Impulse = ImpulseAxe2H;
+        }
+    }
+    private void OnTriggerEnter(Collider col)
+    {
+        
+    }
+}
