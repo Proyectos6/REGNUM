e48ef89c8bf04a21fc6366a1321425d80d2b84f7
inigopanos <inigopanos@gmail.com>
2020-04-27 10:52:07 +0200

    Inventory Master importado Inventario implementado en escena Inventario CUALQUIER DUDA PREGUNTAR A IÑIGO

---
+++ Assets/REGNUM/Scripts/Inventario/Inventario.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventario : MonoBehaviour
+{
+
+    public GameObject panelInventario;
+
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        if (panelInventario.activeInHierarchy == true)
+        {
+            Time.timeScale = 0f;
+            Cursor.visible = true;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            Cursor.visible = false;
+        }
+    }
+}
