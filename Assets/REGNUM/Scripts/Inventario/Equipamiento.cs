44b9909d2261cb11d71308e55b2c73be3d1304de
Iñigo Paños <inigopanos@gmail.com>
2020-05-08 18:53:50 +0200

    Tocando unas cosas pequeñas, preparando el funcionamiento del script

---
+++ Assets/REGNUM/Scripts/Inventario/Equipamiento.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Equipamiento : MonoBehaviour
+{
+    public GameObject player;
+    //public CambioArma cambioArma;   Aquí va el script de cambio de arma del jugador que me ha dicho calde que aún no está
+
+    void Start()
+    {
+        //en el start debe acceder al sistema de equipamiento del jugador y al cambio de arma. Intentar linkearlos
+    }
+
+    void Update()
+    {
+        
+    }
+}
