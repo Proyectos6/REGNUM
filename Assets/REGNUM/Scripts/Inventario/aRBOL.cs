9f183228b63800f9bb4404ff2e83d73721ce94b1
Iñigo Paños <inigopanos@gmail.com>
2020-05-15 22:01:08 +0200

    Mejoras de crafteo e inventario

---
+++ Assets/REGNUM/Scripts/Inventario/aRBOL.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aRBOL : MonoBehaviour
+{
+
+    public GameObject woodDrop;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter (Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Instantiate(woodDrop, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
