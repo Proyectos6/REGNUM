da2e5d2d5e3ab73587ac19088afa9e0a2d4e9352
Iñigo Paños <inigopanos@gmail.com>
2020-05-18 12:01:54 +0200

    -Leña dropeada del árbol recogible una vez -Problemas con el icono al arrastrar el item -Problemas con el número de veces que se puede recoger el item

--- Assets/REGNUM/Scripts/Inventario/PickUpMadera.cs
+++ Assets/REGNUM/Scripts/Inventario/PickUpMadera.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class PickUpMadera : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Item item;
+    [SerializeField]
+    private Inventory inventory;
+    [SerializeField]
+    private GameObject player;
+
+    public GameObject leña;
+
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player_2");
+        if (player != null)
+            inventory = player.GetComponent<PlayerInventory>().inventory.GetComponent<Inventory>();
     }
 
-    // Update is called once per frame
-    void Update()
+   //La diferencia entre este script y el de PickUpItem es que este no depende de la distancia en float si no de si está dentro del trigger o no.
+
+    void OnTriggerStay(Collider col)
     {
-        
+        if (col.CompareTag("Player") && inventory != null && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Entra en contacto la leña con el jugador");
+            if (inventory.ItemsInInventory.Count < (inventory.width * inventory.height))
+            {
+                Debug.Log("Prueba_1");
+                inventory.addItemToInventory(item.itemID, item.itemValue);
+                inventory.updateItemList();
+                inventory.stackableSettings();
+                Destroy(this.gameObject);
+                Debug.Log("Prueba_2");
+            }
+            bool check = inventory.checkIfItemAllreadyExist(item.itemID, item.itemValue);
+            if (check)
+            {
+                Debug.Log("Prueba_3");
+                inventory.addItemToInventory(item.itemID, item.itemValue);
+                inventory.updateItemList();
+                inventory.stackableSettings();
+                Destroy(this.gameObject);
+            }
+           
+        }
     }
 }
+
--- Assets/REGNUM/Scripts/Inventario/aRBOL.cs
+++ Assets/REGNUM/Scripts/Inventario/aRBOL.cs
@@ -4,26 +4,12 @@ using UnityEngine;
 
 public class aRBOL : MonoBehaviour
 {
-
     public GameObject woodDrop;
-
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-       
-    }
 
     void OnTriggerStay(Collider col)
     {      
- 
-
         if (col.CompareTag("Player") && Input.GetKeyDown("f"))
         {
-            Debug.Log("Talando árbol");
             Instantiate(woodDrop, new Vector3(transform.position.x, transform.position.y + 0.2f, transform.position.z), Quaternion.Euler(new Vector3(0, 0, 90)));
             Destroy(gameObject);
         }
