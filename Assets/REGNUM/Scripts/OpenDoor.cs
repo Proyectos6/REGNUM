af4acba78d7dfd7ad1ac171658464c2b1285c158
Jorge_Vera <jorge.vera@live.u-tad.com>
2020-05-21 21:51:18 +0200

    Escena Jorge Pruebas con el nuevo sistema de combate y tres enemigos para probarlo

---
+++ Assets/REGNUM/Scripts/OpenDoor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    Animator animator;
+    
+
+    bool doorOpen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorOpen = false;
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+      if(col.gameObject.tag == "Player")
+        {
+            doorOpen = true;
+            DoorControl("open");
+        } 
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (doorOpen)
+        {
+            doorOpen = false;
+            DoorControl("close");
+        }
+    }
+
+    void DoorControl (string direction)
+    {
+        animator.SetTrigger(direction);
+    }
+}
